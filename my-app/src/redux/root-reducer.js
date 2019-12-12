bb74b4ec8e1b4ffcbbb5c0db0f8419fc5509dfcf
amichalo421 <andre.michalowski@gmail.com>
2019-12-12 14:29:52 +0100

    App component, connect store, logic to reducers

--- my-app/src/redux/root-reducer.js
+++ my-app/src/redux/root-reducer.js
@@ -1,9 +1,33 @@
-export const  INITIAL_STATE = {
-    // initial state here
-  }
+import { START_FETCHING, FETCH_SUCCESS, FETCH_FAILURE } from './actions/index';
+
+export const INITIAL_STATE = {
+    magic: [],
+    isFetching: false,
+    error: ''
+}	
   
   const useReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case START_FETCHING:
+            return {
+              ...state,
+              isFetching: true,
+              error: ''
+            }
+          case FETCH_SUCCESS:
+            return {
+              ...state,
+              isFetching: false,
+              error: '',
+              magic: action.payload
+            }
+          case FETCH_FAILURE:
+            return {
+              ...state,
+              isFetching: false,
+              error: action.payload
+            }
+    
       default:
         return state
     }
