07a192f9b61c5b688a160c2aad8f3189bd74b9e7
amichalo421 <andre.michalowski@gmail.com>
2019-12-12 15:15:48 +0100

    Styling edits

--- my-app/src/components/MagicCard.js
+++ my-app/src/components/MagicCard.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 const MagicCard = ({ magic }) => {
   return (
-    <>
-      <p>{magic[0].name}</p>
-      <img src={magic[0].imageUrl} alt={magic.name} />
-    </>
+    <div className='magic-container'>
+      {Object.values(magic).map(card => (
+    	<div key={card.id} className='card-container'>
+            <img src={card.imageUrl} alt={card.name} />
+                <p>Set: {card.setName}</p>
+                <p>Name: {card.name}</p>
+                <p>Color: {card.colors}</p>
+                <p>{card.text}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
