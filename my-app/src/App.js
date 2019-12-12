bbbe227df2b11443a7a89c33eb58442c600ec028
amichalo421 <andre.michalowski@gmail.com>
2019-12-12 15:03:09 +0100

    Logo added and css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 import Magic from './components/Magic';
@@ -8,6 +8,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          *Search function?*
+        </p>
         <Magic />
       </header>
     </div>
